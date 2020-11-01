677a2cb313174704b9903d81ffe97fcbc07608ab
callbacc <alexa.matei@icloud.com>
2020-11-01 18:14:05 +0200

    TS

--- src/events/ready.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-const { prefix } = require('../Storage/config.json');
-
-module.exports = client => {
-    client.user.setPresence({ activity: { name: "little games :)" }, type: 'WATCHING' });
-}
